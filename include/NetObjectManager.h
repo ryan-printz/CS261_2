1eccec60c98ba7367d162612ce1e900f10eac219
ryan printz <ryan.printz@digipen.edu>
2013-04-26 01:25:01 -0700

    here's a thing

---
+++ include/NetObjectManager.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <unordered_map>
+#include "GameObject.h"
+
+class Asteroids;
+
+typedef std::unordered_map<short, GameObjInst> NetInstContainer;
+
+class NetObjectManager : protected NetInstContainer
+{
+public:
+	NetObjectManager(Asteroids * asgame);
+	~NetObjectManager();
+
+	inline NetInstContainer::iterator begin();
+	inline NetInstContainer::const_iterator begin() const;
+	inline NetInstContainer::iterator end();
+	inline NetInstContainer::const_iterator end() const;
+
+	void push(short netId, unsigned type, unsigned flag, char x, char y, char r);
+	void update(short netId, unsigned type, unsigned flag, char x, char y, char r);
+
+protected:
+	Asteroids * m_asGame;
+};
---
+++ src/NetObjectManager.cpp
@@ -0,0 +1,62 @@
+#include "NetObjectManager.h"
+#include "Asteroids.h"
+
+NetObjectManager::NetObjectManager(Asteroids * asgame)
+	: NetInstContainer(), m_asGame(asgame)
+{}
+
+NetObjectManager::~NetObjectManager()
+{}
+
+NetInstContainer::iterator NetObjectManager::begin()
+{
+	return NetInstContainer::begin();
+}
+
+NetInstContainer::const_iterator NetObjectManager::begin() const
+{
+	return NetInstContainer::begin();
+}
+
+NetInstContainer::iterator NetObjectManager::end()
+{
+	return NetInstContainer::end();
+}
+
+NetInstContainer::const_iterator NetObjectManager::end() const
+{
+	return NetInstContainer::end();
+}
+
+void NetObjectManager::push(short netId, unsigned type, unsigned flag, char x, char y, char r)
+{
+	float scale = 1.0;
+	AEVec2 pos; pos.x = x; pos.y = y;
+
+	switch(type)
+	{
+	case TYPE_SHIP:
+		type = TYPE_NET_SHIP;
+		scale = SHIP_SIZE;
+		break;
+	};
+
+	auto inst = m_asGame->gameObjInstCreate(type, scale, &pos, 0, r, true);
+	emplace(std::make_pair(netId, inst));
+}
+
+void NetObjectManager::update(short netId, unsigned type, unsigned flag, char x, char y, char r)
+{
+	auto inst = find(netId);
+
+	if( inst == end() )
+	{
+		push(netId, type, flag, x, y, r);
+		return;
+	}
+
+	inst->second.flag = flag;
+	inst->second.posCurr.x = x;
+	inst->second.posCurr.y = y;
+	inst->second.dirCurr = r;
+}
